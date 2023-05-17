1a61e674980f5560e6c95f8bd3ca3484a94f14fb
Sean Lee <freshdried@gmail.com>
2023-05-17 02:54:18 -0400

    Fix binding lexical precedence issues.

--- puddlejumper/src/puddlejumper.rs
+++ puddlejumper/src/puddlejumper.rs
@@ -76,7 +76,9 @@ fn pretty_print(node: tree_sitter::Node, input: &str, out: &mut dyn std::io::Wri
 
     loop {
         let n = cursor.node();
-        if n.kind() == "content" {
+        if n.kind() == "content"
+        || n.kind() == "binding"
+        || n.kind() == "ref" {
             write_indent(out, indent_level)?;
             write!(out, "{}\n", &input[n.start_byte()..n.end_byte()])?;
         }
@@ -153,8 +155,19 @@ mod tests {
         assert!(result.is_ok());
         let output = String::from_utf8(output).unwrap();
         assert_eq!(output, "hello\n\n  world")
-
     }
+
+    #[test]
+    fn test_lossless_print_bindings() {
+        let code = String::from("@hello:\n@world");
+        let p = Parser::new(code);
+        let mut output = Vec::new();
+        let result = p.lossless_print(&mut output);
+        assert!(result.is_ok());
+        let output = String::from_utf8(output).unwrap();
+        assert_eq!(output, "@hello:\n\n\n\n@world")
+    }
+
     #[test]
     fn test_pretty_print() {
         let code = String::from("hello\n\n  world");
@@ -164,5 +177,16 @@ mod tests {
         assert!(result.is_ok());
         let output = String::from_utf8(output).unwrap();
         assert_eq!(output, "hello\n    world\n")
+    }
+
+    #[test]
+    fn test_pretty_print_bindings() {
+        let code = String::from("@hello:\n@world");
+        let p = Parser::new(code);
+        let mut output = Vec::new();
+        let result = p.pretty_print(&mut output);
+        assert!(result.is_ok());
+        let output = String::from_utf8(output).unwrap();
+        assert_eq!(output, "@hello:\n@world\n")
     }
 }
--- tree-sitter-puddlejumper/grammar.js
+++ tree-sitter-puddlejumper/grammar.js
@@ -1,30 +1,33 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
 module.exports = grammar({
   name: "puddlejumper",
-  externals: $ => [
-    $.indent,
-    $.dedent,
-    $.newline,
-    $.mega_newline,
-  ],
+  externals: ($) => [$.indent, $.dedent, $.newline, $.mega_newline],
+  precedences: ($) => [["binding", "ref"]],
+  extras: ($) => [],
+  word: ($) => $.identifier,
   rules: {
-    document: $ => optional($._stanza),
-    _stanza: $ => repeat1(choice(
-      $.node,
-      $.block_node,
-    )),
+    document: ($) => optional($._stanza),
+    _stanza: ($) =>
+      repeat1(
+        choice(
+          seq($._primary_node, optional($.newline)),
+          seq($.block_node, optional($.mega_newline))
+        )
+      ),
+    _primary_node: ($) => choice($.node, $.ref_node),
 
-    block_node: $ => seq($.binding, repeat1(seq($.newline, $.node)), $.mega_newline),
+    block_node: ($) => seq($.binding, /\s*/, repeat1(seq($.newline, $.node))),
+    node: ($) =>
+      seq(optional(seq($.binding, /\s*/)), $.content, optional($.children)),
+    ref_node: ($) => seq($.ref, optional($.children)),
+    children: ($) => seq($.indent, $._stanza, $.dedent),
 
-    node: $ => prec.left(seq(optional(seq($.binding, /\s*/)), $.content, optional(choice($.children, $.newline)))),
-    children: $ => seq(
-      $.indent,
-      $._stanza,
-      $.dedent,
-    ),
-
-    identifier: $ => /[a-zA-Z0-9_]+/,
-    binding: $ => seq("@", optional($.identifier), ":"),
-    ref: $ => seq("@", $.identifier),
-    content: $ => /[^@ \n][^\n]*/,
-  }
+    identifier: ($) => /[a-zA-Z0-9_]+/,
+    binding: ($) =>
+      seq(token(prec("binding", "@")), optional($.identifier), ":"),
+    ref: ($) => seq(token(prec("ref", "@")), $.identifier),
+    _node_content: ($) => choice($.content, $.ref),
+    content: ($) => /[^@ \n][^\n]*/,
+  },
 });
