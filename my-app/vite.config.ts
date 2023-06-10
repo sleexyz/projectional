ff15d251ef80c3801c78feb15827c6029a1b470e
Sean Lee <freshdried@gmail.com>
2023-06-10 01:01:30 -0400

    Support ibazel run with hot reload.

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,9 +3,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import wasm from "vite-plugin-wasm";
+import * as childProcess from "child_process";
+
+const reloadOnStdin= () => ({
+  name: 'reload-on-stdin',
+  handleHotUpdate({ file, server }) {
+    console.log("handleHotUpdate: ", file);
+  },
+  configureServer(server) {
+    process.stdin.on('data', data => {
+      const msgs = new Set(data.toString().split("\n"));
+      // TODO: Move this to bazel rule.
+      if (msgs.has("IBAZEL_BUILD_COMPLETED SUCCESS")) {
+        // Run START_SCRIPT --no-exec to completion:
+        childProcess.execSync(process.env["START_SCRIPT"] + " --no-exec", {stdio: 'inherit'});
+      }
+    });
+  }
+})
 
 export default defineConfig({
-  plugins: [wasm(), react(), reactClickToComponent()],
+  plugins: [wasm(), react(), reactClickToComponent(), reloadOnStdin()],
   build: {
     rollupOptions: {
       input: {
