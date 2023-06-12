35e82ea3e4aa965c1c5b9dbe1b34d494bde4a8a4
Sean Lee <freshdried@gmail.com>
2023-06-11 23:43:47 -0400

    Cleanup.

--- my-app/src/lib.rs
+++ my-app/src/lib.rs
@@ -1,8 +1,8 @@
-use ::puddlejumper::puddlejumper::node::{NodeId, Context};
+use ::puddlejumper::puddlejumper::node::{Context, NodeId};
+use puddlejumper::puddlejumper;
+use std::io::{Error, ErrorKind};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use std::io::{Error, ErrorKind};
-use puddlejumper::puddlejumper;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -12,7 +12,6 @@ use puddlejumper::puddlejumper;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -21,28 +20,30 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    let Some((context, foo)) = parse("Hello\n\tP1\n\t\thello") else { return Ok(()) };
+    let Some((context, foo)) = parse("Hello\n\tP1\n\t\thullo") else { return Ok(()) };
 
     let mut out = Vec::new();
-    context.pretty_print(foo, &mut puddlejumper::node::printer::PrintContext {
-        level: 0,
-        needs_indent: true,
-        out: &mut out,
-    });
-    console::log_1(&JsValue::from_str(&String::from_utf8(out).unwrap()));
+    context.pretty_print(
+        foo,
+        &mut puddlejumper::node::printer::PrintContext {
+            level: 0,
+            needs_indent: true,
+            out: &mut out,
+        },
+    );
+    unsafe { console::log_1(&JsValue::from_str(&String::from_utf8(out).unwrap())) };
     Ok(())
 }
 
-fn parse(code: &str) -> Option<(Context, NodeId)>  {
+fn parse(code: &str) -> Option<(Context, NodeId)> {
     let p = puddlejumper::parser::Parser::new(code.into());
     p.load_document()
 }
 
-fn print_prioritized(code: &str) -> Option<String>  {
+fn print_prioritized(code: &str) -> Option<String> {
     let p = puddlejumper::parser::Parser::new(code.into());
     let mut output = Vec::new();
-    p
-        .load_document()
+    p.load_document()
         .ok_or(Error::new(ErrorKind::Other, "Error parsing file"))
         .and_then(|(mut ctx, node)| {
             let list = ctx.make_prioritized_list(node);
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,33 +3,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import wasm from "vite-plugin-wasm";
-import * as childProcess from "child_process";
 
-const reloadOnStdin= () => ({
-  name: 'reload-on-stdin',
+const reloadOnStdin = () => ({
+  name: "reload-on-stdin",
   handleHotUpdate({ file, server }) {
     console.log("handleHotUpdate: ", file);
     if (file.endsWith(".html")) {
       // ignore update
-      return [];
+      // return [];
     }
   },
   configureServer(server) {
-    process.stdin.on('data', data => {
+    process.stdin.on("data", (data) => {
       const msgs = new Set(data.toString().split("\n"));
-      // TODO: Move this to bazel rule.
       if (msgs.has("IBAZEL_BUILD_COMPLETED SUCCESS")) {
         console.log("reloading");
-        // Run START_SCRIPT --no-exec to completion:
-        // childProcess.execSync(process.env["START_SCRIPT"] + " --no-exec", {stdio: 'inherit'});
       }
     });
-  }
-})
+  },
+});
 
 export default defineConfig({
   plugins: [wasm(), react(), reactClickToComponent(), reloadOnStdin()],
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
